19571ec6a28e0e818c6318a830015e01a9ca807e
emirnur <nur.emirlan@gmail.com>
2019-09-21 13:09:43 +0600

    add dump

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from webapp.models import HostBook
 
-# Register your models here.
+
+class HostBookAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'email', 'status']
+    list_filter = ['name', 'status']
+    list_display_links = ['pk', 'name']
+    search_fields = ['name', 'text']
+    fields = ['name', 'email', 'text', 'status', 'created_at', 'updated_at']
+    readonly_fields = ['created_at', 'updated_at']
+
+
+admin.site.register(HostBook, HostBookAdmin)
